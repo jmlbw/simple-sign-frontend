f2e3f7f94da2f9cc98917d6e362c28434eacb700
jmlbc <jmlbcad@gmail.com>
2023-09-19 16:20:40 +0900

    Feat: 회사명 api 추가

--- src/components/formManage/searchBox/components/SearchItem.js
+++ src/components/formManage/searchBox/components/SearchItem.js
@@ -1,15 +1,6 @@
-import { useState, useEffect } from 'react';
 import styled from '../../../../styles/components/formManage/searchBox/components/SearchItem.module.css';
 
 export default function SearchItem({ asset1, asset2, data }) {
-  const [selectList, setSelectList] = useState([]);
-
-  useEffect(() => {
-    if (data.length > 1) {
-      setSelectList(data);
-    }
-  }, []);
-
   const InputComp = ({ type }) => {
     return (
       <div className={styled.inputBox}>
@@ -21,8 +12,8 @@ export default function SearchItem({ asset1, asset2, data }) {
   const SelectComp = ({ width = 170 }) => {
     return (
       <select className={styled.select} style={{ width: `${width}px` }}>
-        {selectList.length !== 0
-          ? selectList.map((ele, index) => {
+        {data.length !== 0
+          ? data.map((ele, index) => {
               return (
                 <option key={index} value={ele.name}>
                   {ele.name}
@@ -33,17 +24,6 @@ export default function SearchItem({ asset1, asset2, data }) {
       </select>
     );
   };
-
-  // const searchSelectSub = () => {
-  //   switch (type) {
-  //     case 'text':
-  //       return <InputComp />;
-  //     case 'select':
-  //       return <SelectComp />;
-  //     default:
-  //       return null;
-  //   }
-  // };
 
   const assetRender = (asset) => {
     if (asset === 'select') {
@@ -59,17 +39,6 @@ export default function SearchItem({ asset1, asset2, data }) {
 
   return (
     <div className={styled.itemBox}>
-      {/* {title === 'select' ? (
-        <>
-          <SelectComp width={100} /> <InputComp />
-        </>
-      ) : (
-        <>
-          <p className={styled.title}>{title}</p>
-          {searchSelectSub()}
-        </>
-      )} */}
-
       {assetRender(asset1)}
       {assetRender(asset2)}
     </div>
