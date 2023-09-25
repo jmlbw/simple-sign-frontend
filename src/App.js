6cae6801fb3d8432e2f0a93c2ebd2a8652f06a38
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-09-25 13:12:01 +0900

    feat : 로그인 켄텍스트

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContextProvider from './contexts/ContextProvider';
@@ -14,32 +15,57 @@ import SeqManagePage from './pages/SeqManagePage';
 
 import FormListPage from './pages/FormListPage';
 
+import AppContext from './contexts/AppContext';
+import Login from './pages/Login';
 
+function AppContent() {
+  const { state } = useContext(AppContext);
+  const navigate = useNavigate();
 
+  //로그인이 되지 않으면 로그인 페이지로
+  useEffect(() => {
+    if (!state.isLoggedIn) {
+      navigate('/login');
+    }
+  }, [state.isLoggedIn, navigate]);
+
+  return state.isLoggedIn ? (
+    //로그인이 되었을 때 모든 페이지
+    <>
+      <Header />
+      <Sidebar />
+      <div className="contentContainer">
+        <Titlebox
+          title="상신문서"
+          view="approval"
+          componentProp={<ApprovalRightHeader />}
+        ></Titlebox>
+        <div className="contentsArea">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/EAM" element={<FormManagePage />} />
+            <Route path="/ABV" element={<ApprovalBoxViewPage />} />
+            <Route path="/SAM" element={<SeqManagePage />} />
+            <Route path="/FL" element={<FormListPage />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  ) : (
+    //로그인이 되지 않았을 때 로그인 페이지
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <ContextProvider>
       <div className="App">
         <BrowserRouter>
-          <Header />
-          <Sidebar />
-          <div className="contentContainer">
-            <Titlebox
-              title="상신문서"
-              view="approval"
-              componentProp={<ApprovalRightHeader />}
-            ></Titlebox>
-            <div className="contentsArea">
-              <Routes>
-                <Route path="/" element={<HomePage></HomePage>} />
-                <Route path="/EAM" element={<FormManagePage />} />
-                <Route path="/ABV" element={<ApprovalBoxViewPage />} />
-                <Route path="/SAM" element={<SeqManagePage />} />
-                <Route path="/FL" element={<FormListPage />} />
-              </Routes>
-            </div>
-          </div>
+          <AppContent />
         </BrowserRouter>
       </div>
     </ContextProvider>
