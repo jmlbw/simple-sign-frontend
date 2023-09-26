467f7eeafc56ebd2fddcbf4fc8485154018ce7e6
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-09-26 10:53:21 +0900

    Feat: 양식 삭제 기능 추가

--- src/components/formManage/formList/DataList.js
+++ src/components/formManage/formList/DataList.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { useFormManage } from '../../../contexts/FormManageContext';
 
@@ -6,18 +6,12 @@ export default function DataList({ columns, rows, dataHandler }) {
   const { updateDetailData } = useFormManage();
 
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div style={{ height: '100%', width: '100%' }}>
       <DataGrid
         autoHeight
         rows={rows}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
+        disableSelectionOnClick
         onRowClick={(event) => {
           dataHandler(event.row);
           updateDetailData();
