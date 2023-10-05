7d6f2af664c37b8572918828cc5c10aed2855dda
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-05 11:29:03 +0900

    Feat: 채번 양식팝업 추가

--- src/assets/datas/form_manage_list.js
+++ src/assets/datas/form_manage_list.js
@@ -3,11 +3,3 @@ export const columns = [
   { field: 'formName', headerName: '양식명', width: 150 },
   { field: 'status', headerName: '사용여부', width: 70 },
 ];
-
-// const rows = [
-//   { id: 1, compName: '더존비즈온', formName: '지각 사유서', status: '사용' },
-//   { id: 2, compName: '더존비즈온', formName: '휴가 신청서', status: '사용' },
-//   { id: 3, compName: '비트컴퓨터', formName: '보고서', status: '미사용' },
-//   { id: 4, compName: '비트컴퓨터', formName: '결재서', status: '사용' },
-//   { id: 5, compName: '비트컴퓨터', formName: '무슨 신청서', status: '사용' },
-// ];
