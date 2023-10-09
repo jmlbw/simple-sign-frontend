928e8f7228e0b89344d6c97986fa700fa574cd3b
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-09 18:04:46 +0900

    Feat: 메인 페이지 화면 분할

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,14 @@
-import '../styles/pages/HomePage.css';
+import styled from '../styles/pages/HomePage.module.css';
 import React from 'react';
 
 export default function HomePage() {
-  return <div>homepage</div>;
+  return (
+    <div className={styled.container}>
+      <div className={styled.recoFormArea}>추천양식</div>
+      <div className={styled.favoriteArea}>즐겨찾기</div>
+      <div className={styled.processDocArea}>진행문서</div>
+      <div className={styled.receRefArea}>받은수신</div>
+      <div className={styled.timeLineArea}>타임라인</div>
+    </div>
+  );
 }
