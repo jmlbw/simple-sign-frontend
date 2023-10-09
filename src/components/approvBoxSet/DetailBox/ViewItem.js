bfec42bb2098c158dae7121936a21eacbbac559f
siyeonee <rodnjsqufdl@naver.com>
2023-10-09 21:46:38 +0900

    feat: 조회항목 가져오기

--- src/components/approvBoxSet/DetailBox/ViewItem.js
+++ src/components/approvBoxSet/DetailBox/ViewItem.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ViewItemPopup from '../ViewItemPopup';
 import Optionbox from '../../common/Optionbox';
 import styled from '../../../styles/pages/ApprovalBoxSetPage.module.css';
+import getViewItems from '../../../apis/approvalBoxAPI/getViewItems';
+import { useApprovalBox } from '../../../contexts/ApprovalBoxContext';
+
+const BASE_RADIX = 10;
 
 function ViewItem(props) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const { state, setState } = useApprovalBox();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const parsedBoxId = parseInt(props.boxId, BASE_RADIX);
+        if (!isNaN(parsedBoxId)) {
+          const response = await getViewItems(parsedBoxId);
+          setData(response.data);
+
+          let updatedViewItem = [];
+
+          response.data.forEach((viewItem) => {
+            if (viewItem.codeId.includes('01')) {
+              console.log(viewItem.codeId);
+              updatedViewItem.push('send');
+            } else if (viewItem.codeId.includes('02')) {
+              updatedViewItem.push('pend');
+            } else if (viewItem.codeId.includes('03')) {
+              updatedViewItem.push('concluded_end');
+            } else if (viewItem.codeId.includes('04')) {
+              updatedViewItem.push('concluded_progress');
+            } else if (viewItem.codeId.includes('05')) {
+              updatedViewItem.push('rejected');
+            } else if (viewItem.codeId.includes('06')) {
+              updatedViewItem.push('reference');
+            }
+          });
+
+          setState((prevState) => ({
+            ...prevState,
+            boxViewItems: updatedViewItem,
+          }));
+        }
+      } catch (err) {
+        setError(err);
+        console.error('Error fetching box details:', err);
+      }
+    };
+    fetchData();
+  }, [props.boxId]);
+
   return (
     <div className={styled.inputItem}>
       <div style={props.commonCellStyle}>
@@ -12,12 +60,16 @@ function ViewItem(props) {
       <div style={props.commonDataStyle}>
         <div className={styled.viewUseField}>
           <div className={styled.viewItemBox}>
-            <Optionbox />
+            {data.map((viewItem) => (
+              <Optionbox key={viewItem.codeId} name={viewItem.codeValue} />
+            ))}
           </div>
+
           <ViewItemPopup />
         </div>
       </div>
     </div>
   );
 }
+
 export default ViewItem;
