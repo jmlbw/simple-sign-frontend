956bf68f68bbbdc107aac30ed8984ddeaa14d0c0
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-10 10:13:07 +0900

    Feat: 홈페이지 레이아웃

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,29 @@
 import styled from '../styles/pages/HomePage.module.css';
 import React from 'react';
+import RecommendForm from '../components/home/RecommendForm';
+import Favorite from '../components/home/Favorite';
+import ProgressDoc from '../components/home/ProgressDoc';
+import ReceivedRef from '../components/home/ReceivedRef';
+import TimeLine from '../components/home/TimeLine';
 
 export default function HomePage() {
   return (
     <div className={styled.container}>
-      <div className={styled.recoFormArea}>추천양식</div>
-      <div className={styled.favoriteArea}>즐겨찾기</div>
-      <div className={styled.processDocArea}>진행문서</div>
-      <div className={styled.receRefArea}>받은수신</div>
-      <div className={styled.timeLineArea}>타임라인</div>
+      <div className={styled.recoFormArea}>
+        <RecommendForm />
+      </div>
+      <div className={styled.favoriteArea}>
+        <Favorite />
+      </div>
+      <div className={styled.processDocArea}>
+        <ProgressDoc />
+      </div>
+      <div className={styled.receRefArea}>
+        <ReceivedRef />
+      </div>
+      <div className={styled.timeLineArea}>
+        <TimeLine />
+      </div>
     </div>
   );
 }
