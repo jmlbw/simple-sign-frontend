a210eb9cea912245f598378a83470a35a03d4b9a
brightvvater <hscho199463@gmail.com>
2023-10-15 01:00:45 +0900

    fix: fetch credentials 추가

--- src/apis/approvalManageAPI/getApprovalDoc.js
+++ src/apis/approvalManageAPI/getApprovalDoc.js
@@ -1,6 +1,9 @@
 export default function getApprovalDoc(approval_doc_id) {
   let url = `http://localhost:8080/approve/detail/${approval_doc_id}`;
-  return fetch(url, { headers: { Accept: 'application/json' } }).then((res) => {
+  return fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  }).then((res) => {
     return res.json();
   });
 }
--- src/apis/approvalManageAPI/insertReturn.js
+++ src/apis/approvalManageAPI/insertReturn.js
@@ -1,6 +1,7 @@
 export default function insertReturn(page) {
   let url = `http://localhost:8080/approve/return/${page}`;
   return fetch(url, {
-    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
   });
 }
