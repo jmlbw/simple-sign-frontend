e3a2792cf2aa0cf5b81318bb4da84cb2a81c31e5
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-10-17 17:14:13 +0900

    feat: 프로필 서명 조회 및 수정

---
+++ src/apis/userInfoAPl/getSign.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import base_url from '../base_url';
+
+const api = axios.create({
+  baseURL: base_url,
+});
+
+export const getSign = () => {
+  return api.get('/userinfo/sign');
+};
+
+export const getUpdateSign = () => {
+  return api.get('/updateinfo/sign');
+};
--- src/pages/UpdateUserInfo.js
+++ src/pages/UpdateUserInfo.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import InnerBox from '../../components/common/InnerBox';
-import Button from '../../components/common/Button';
+import InnerBox from '../components/common/InnerBox';
+import Button from '../components/common/Button';
 import { useNavigate, useLocation } from 'react-router';
-import putUserInfo from '../../apis/userInfoAPl/putUserInfo';
-import PopUp from '../../components/common/PopUp';
-import PopUpFoot from '../../components/common/PopUpFoot';
-import UserPWChange from '../../components/userinfo/UserPWChange';
-import postPassword from '../../apis/userInfoAPl/postPassword';
+import putUserInfo from '../apis/userInfoAPl/putUserInfo';
+import PopUp from '../components/common/PopUp';
+import PopUpFoot from '../components/common/PopUpFoot';
+import UserPWChange from '../components/userinfo/UserPWChange';
+import postPassword from '../apis/userInfoAPl/postPassword';
+import { postProfile } from '../apis/userInfoAPl/postProfile';
+import { postSign } from '../apis/userInfoAPl/postSign';
 import DaumPostcode from 'react-daum-postcode';
 import Radio from '@mui/material/Radio';
-import styled from '../../styles/pages/UpdateUserInfo.module.css';
+import styled from '../styles/pages/UpdateUserInfo.module.css';
 
 export default function UpdateUserInfo() {
   const location = useLocation();
@@ -38,7 +40,19 @@ export default function UpdateUserInfo() {
       ? location.state.userData
       : initialUserData;
 
+  const initialprofile =
+    location && location.state && location.state.profile
+      ? location.state.profile
+      : null;
+
+  const initialSign =
+    location && location.state && location.state.sign
+      ? location.state.sign
+      : null;
+
   const [userData, setUserData] = useState(userDataFromLocation);
+  const [profile, setProfile] = useState(initialprofile);
+  const [sign, setSign] = useState(location.state.dbSign);
 
   const [pwdData, setPwdData] = useState({
     currentPassword: '',
@@ -144,25 +158,82 @@ export default function UpdateUserInfo() {
       });
   };
 
-  // 업데이트하는 api
+  // 라디오 버튼
+  const [selectedValue, setSelectedValue] = React.useState(
+    initialSign === 'default' ? 'D' : 'C'
+  );
+
+  // 프로필
+  const [profileFile, setProfileFile] = useState(null);
+
+  // 서명
+  const [currentSign, setCurrentSign] = useState(location.state.dbSign);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleProfileChange = (e) => {
+    const file = e.target.files[0];
+    setProfileFile(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProfile(reader.result);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSignChange = (e) => {
+    const file = e.target.files[0];
+    setCurrentSign(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setSign(reader.result);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   const updateAPI = () => {
-    putUserInfo(userData)
+    const promises = [];
+
+    // 프로필 업로드
+    if (profileFile) {
+      const formData = new FormData();
+      formData.append('file', profileFile);
+      promises.push(postProfile(formData));
+    }
+
+    // 서명 업로드
+    if (selectedValue) {
+      const formData = new FormData();
+      if (selectedValue === 'C' && currentSign) {
+        formData.append('signState', 1);
+        formData.append('file', currentSign);
+      } else {
+        formData.append('signState', 0);
+      }
+      promises.push(postSign(formData));
+    }
+
+    // 개인 정보 업데이트
+    Promise.all(promises)
+      .then(() => {
+        return putUserInfo(userData);
+      })
       .then((response) => {
         if (response.status === 200) {
           navigate('/userinfo');
         }
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error);
       });
-  };
-
-  // 라디오 버튼
-  const [currentSign, setCurrentSign] = useState(0);
-  const [selectedValue, setSelectedValue] = React.useState('a');
-
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
   };
 
   return (
@@ -179,29 +250,45 @@ export default function UpdateUserInfo() {
             <tr>
               <th className={styled.userinfo_table_th}>프로필</th>
               <td>
-                <img className={styled.profile_img} alt="프로필" />
-                <input type="file" />
+                <div className={styled.profile_container}>
+                  <img
+                    className={styled.profile_img}
+                    src={profile}
+                    alt="프로필"
+                  />
+                  <input type="file" onChange={handleProfileChange} />
+                </div>
               </td>
               <th className={styled.userinfo_table_th}>서명</th>
               <td>
-                <div className={styled.sign_img}>
-                  <Radio
-                    checked={selectedValue === 'a'}
-                    onChange={handleChange}
-                    value="a"
-                    name="radio-buttons"
-                    inputProps={{ 'aria-label': 'A' }}
-                  />
-                  <label>디폴트</label>
-
-                  <Radio
-                    checked={selectedValue === 'b'}
-                    onChange={handleChange}
-                    value="b"
-                    name="radio-buttons"
-                    inputProps={{ 'aria-label': 'B' }}
-                  />
-                  <label>커스텀</label>
+                <div className={styled.sign_img_container}>
+                  <div className={styled.sign_radio_container}>
+                    <Radio
+                      checked={selectedValue === 'D'}
+                      onChange={handleChange}
+                      value="D"
+                      name="radio-buttons"
+                      inputProps={{ 'aria-label': 'D' }}
+                    />
+                    <div>
+                      <p className={styled.default_sign}>{userData.userName}</p>
+                    </div>
+                  </div>
+                  <div className={styled.sign_custom_container}>
+                    <Radio
+                      checked={selectedValue === 'C'}
+                      onChange={handleChange}
+                      value="C"
+                      name="radio-buttons"
+                      inputProps={{ 'aria-label': 'C' }}
+                    />
+                    <img src={sign} alt="사인" className={styled.sign_img} />
+                    <input
+                      type="file"
+                      onChange={handleSignChange}
+                      className={styled.sign_input}
+                    />
+                  </div>
                 </div>
               </td>
             </tr>
