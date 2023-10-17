18163f80d4db727a93918fd4975d7dcd408e546e
brightvvater <hscho199463@gmail.com>
2023-10-17 13:18:31 +0900

    feat: 댓글 수정 및 삭제 API 연결

--- src/components/approvalManage/approvalDetail/ReplyDetail.js
+++ src/components/approvalManage/approvalDetail/ReplyDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from '../../../styles/components/approvalManage/approvalDetail/ReplyDetail.module.css';
 // import Button from '../../common/Button';
 import { Avatar } from '@mui/material';
@@ -7,6 +7,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import { styled as MUIStyled } from '@mui/material';
+import { json } from 'react-router-dom';
 
 const CustomButton = MUIStyled(Button)({
   width: '0.5em',
@@ -26,6 +27,85 @@ export default function ReplyDetail({
   isSecondDept,
   handleInsertReply,
 }) {
+  const contentEditableRef = useRef(content);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedContent, setEditedContent] = useState(content);
+
+  const updateHandler = () => {
+    //권한가져오고 권한이 있으면 contentEditable로 바꿔주기
+    fetch(`http://localhost:8080/reply/isEdit/${replyId}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setIsEdit(res);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  const deleteHandler = (replyId) => {
+    fetch(`http://localhost:8080/reply/${replyId}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    })
+      .then((res) => {
+        if (res.status == '200') {
+          alert('댓글이 삭제되었습니다.');
+        } else {
+          alert('댓글 삭제 실패');
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  const updateContentHandler = (replyId) => {
+    // console.log(editedContent);
+    // console.log(replyId);
+    const data = {
+      replyContent: editedContent,
+    };
+    fetch(`http://localhost:8080/reply/${replyId}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status == '200') {
+          alert('댓글이 수정되었습니다.');
+        } else {
+          alert('댓글 수정 실패');
+        }
+      })
+      .catch((e) => {
+        alert('댓글 수정 실패');
+        console.error(e);
+      });
+  };
+
+  useEffect(() => {
+    if (isEdit) {
+      contentEditableRef.current.focus();
+    }
+  });
+
+  const cancelHandler = () => {
+    setIsEdit(false);
+  };
+
+  const handleContentChange = (e) => {
+    setEditedContent(e.target.value);
+  };
+
   return (
     <div
       className={
@@ -54,28 +134,59 @@ export default function ReplyDetail({
             </div>
             <br />
             <br />
-            <div style={{ textAlign: 'left' }}>{content}</div>
+            {isEdit ? (
+              <>
+                <input
+                  className={styled.inputBox}
+                  ref={contentEditableRef}
+                  value={editedContent}
+                  onChange={handleContentChange}
+                />
+                <CustomButton
+                  size="small"
+                  onClick={() => {
+                    updateContentHandler(replyId);
+                  }}
+                >
+                  수정
+                </CustomButton>
+                <span>|</span>
+                <CustomButton
+                  size="small"
+                  onClick={() => {
+                    cancelHandler();
+                  }}
+                >
+                  취소
+                </CustomButton>
+              </>
+            ) : (
+              <div className={styled.div}>{content}</div>
+            )}
           </CardContent>
+
           <div style={{ display: 'block' }}>
-            <div style={{ float: 'left' }}>
-              <CustomButton
-                size="small"
-                onClick={() => {
-                  handleInsertReply(index);
-                }}
-              >
-                수정
-              </CustomButton>
-              <span>|</span>
-              <CustomButton
-                size="small"
-                onClick={() => {
-                  handleInsertReply(index);
-                }}
-              >
-                삭제
-              </CustomButton>
-            </div>
+            {isEdit ? null : (
+              <div style={{ float: 'left' }}>
+                <CustomButton
+                  size="small"
+                  onClick={() => {
+                    updateHandler(replyId);
+                  }}
+                >
+                  수정
+                </CustomButton>
+                <span>|</span>
+                <CustomButton
+                  size="small"
+                  onClick={() => {
+                    deleteHandler(replyId);
+                  }}
+                >
+                  삭제
+                </CustomButton>
+              </div>
+            )}
             <CardActions style={{ display: 'contents' }}>
               <div className={isSecondDept ? styled.hideReplyContent : ''}>
                 <div style={{ float: 'right' }}>
