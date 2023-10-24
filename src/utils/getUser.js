7bba26e5db80f7f0181d5e1ef334c0f5310a0710
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-10-24 10:17:40 +0900

    feat: 로컬에 저장한 값 함수화

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -6,8 +6,37 @@ const getCompId = () => {
   return localStorage.getItem('compId');
 };
 
+const getCompName = () => {
+  return localStorage.getItem('compName');
+};
+
+const getDeptId = () => {
+  return localStorage.getItem('deptId');
+};
+
+const getDeptName = () => {
+  return localStorage.getItem('deptName');
+};
+
 const getOrgUserId = () => {
   return localStorage.getItem('orgUserId');
 };
 
-export { getAuthrity, getCompId, getOrgUserId };
+const getUserId = () => {
+  return localStorage.getItem('userId');
+};
+
+const getUserName = () => {
+  return localStorage.getItem('userName');
+};
+
+export {
+  getAuthrity,
+  getCompId,
+  getCompName,
+  getDeptId,
+  getDeptName,
+  getOrgUserId,
+  getUserId,
+  getUserName,
+};
