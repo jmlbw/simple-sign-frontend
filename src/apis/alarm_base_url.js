f6626a061ee91846ba957f47032443417c542675
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-11-14 13:50:39 +0900

    fix: 알림 베이스 URL 변경

--- src/apis/alarm_base_url.js
+++ src/apis/alarm_base_url.js
@@ -1,4 +1,4 @@
 const alarm_base_url =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/';
+  process.env.REACT_APP_ALARM_BASE_URL || 'http://localhost:8081/';
 
 export default alarm_base_url;
